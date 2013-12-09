1d62121e2388ac1818d18a99fa29bd158c1c4339
DaspawnW <bjoern-wenzel@hotmail.de>
2013-12-09 12:46:23 +0100

    REV1

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/html'));
 
+app.get('/firstCall', function(request, response) {
+	response.send('answer');
+});
+
+app.post()
+
 app.listen(8080);
